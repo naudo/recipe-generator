b6f9d0b628b76ae153b8c059e8302ff596e67c6e
Nicholas Audo <naudo@naudo.de>
2013-06-18 16:18:36 -0700

    add sample script

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ recipe-generator
 ================
 
 A quick and dirty intro to markov chains.  Uses the official whitehouse cookbook from projectgutenberg 
+
+This will generate awesome recipes as 
+- e fine and stirred into a tureen containing a gill of milk, four eggs,
+  beat them in, by beating
+-be fine and not allowed to rise to the taste of essence of spruce; when
+cool, flavor; then pour
+
+## Run
+
+python markov.py wh.txt
+
+
+
+
+
---
+++ markov.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys, random
+
+def make_chains(corpus):
+    """Takes an input text as a string and returns a dictionary of
+    markov chains."""
+
+    chains = {}
+    #split() will default to split(" ") - awesome
+    word_list = corpus.split()
+
+
+    for i in range(len(word_list) - 2):
+      first_word = word_list[i]
+      second_word = word_list[i + 1]
+      trailing_word = word_list[i +2]
+
+      index_tuple = (first_word, second_word)
+
+      chains.setdefault(index_tuple, [])
+      chains[index_tuple].append(trailing_word)
+
+    return chains
+
+def make_text(chains):
+    """Takes a dictionary of markov chains and returns random text
+    based off an original text."""
+    text_array = []
+    size = 20
+
+    seed = random.randint(0, size - 3)# 3 because we want to exclude seed word, next word, and trailing word    
+
+    seed_word = "be" 
+    next_word = "fine"
+ 
+    for i in range(1,size):
+      text_array.append(seed_word)
+      trailing_word = random.choice(chains[seed_word, next_word])
+
+      seed_word = next_word
+      next_word = trailing_word
+
+    return " ".join(text_array)
+
+def main():
+    args = sys.argv
+    file_path = args[1]
+    file = open(file_path)
+    corpus = file.read()
+
+    chain_dict = make_chains(corpus)
+    random_text = make_text(chain_dict)
+
+    print random_text
+
+if __name__ == "__main__":
+    main()
